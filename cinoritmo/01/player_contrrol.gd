87e78e47495f102ad71bd367d4a3e7516653999d
Takacs Artur <takacs.artur@outlook.com>
2025-02-25 22:55:17 +0100

    minumum viablle product :D Finna ship this heap m8 fakin ell

--- cinoritmo/01/player_contrrol.gd
+++ cinoritmo/01/player_contrrol.gd
@@ -1,15 +1,38 @@
 extends CharacterBody2D
-var velocityy = 120
+var curentVelocity = 200
+var score = 0
+var inside = false
+var intopRow = false
 
 func _physics_process(delta: float) -> void:
 	#going right 'ere
-	velocity.x = velocityy
+	velocity.x = curentVelocity
 	move_and_slide()
 
 func _on_end_area_body_entered(body: Node2D) -> void:
-	print("Exited") # Replace with function body.
-
-
+	curentVelocity = 0
+	
 func _on_start_area_body_entered(body: Node2D) -> void:
 	print("Entered")
-	velocityy = 60
+	curentVelocity = 60
+
+func _on_top_row_body_entered(body: Node2D) -> void:
+	intopRow = true
+	print("EnteredTopRow")
+func _on_top_row_body_exited(body: Node2D) -> void:
+	intopRow = false
+	print("ExitedTopRow")
+
+func _process(delta: float) -> void:
+	if intopRow:
+		if Input.is_action_pressed("toprow"):
+			if Input.is_action_pressed("strum"):
+				print("moreinfo")
+				increaseScore()	
+
+func faultyPlay() -> void:
+	pass
+	
+func increaseScore() -> void:
+	score += 100
+	print(score)
