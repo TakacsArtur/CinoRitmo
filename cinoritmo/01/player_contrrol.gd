0b889d42386058a24b3ce6ed5e89cf3f4e2776bf
TyR97 <vargajanos97@gmail.com>
2025-02-25 22:28:23 +0100

    BigChanges

--- cinoritmo/01/player_contrrol.gd
+++ cinoritmo/01/player_contrrol.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
+var velocityy = 120
 
 func _physics_process(delta: float) -> void:
 	#going right 'ere
-	velocity.x = 90
+	velocity.x = velocityy
 	move_and_slide()
 
 func _on_end_area_body_entered(body: Node2D) -> void:
@@ -11,3 +12,4 @@ func _on_end_area_body_entered(body: Node2D) -> void:
 
 func _on_start_area_body_entered(body: Node2D) -> void:
 	print("Entered")
+	velocityy = 60
