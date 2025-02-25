d0f21720ca0e1de8a3cb954910547615f5a644ac
Takacs Artur <takacs.artur@outlook.com>
2025-02-25 21:33:18 +0100

    created basic scripts

--- cinoritmo/01/player_contrrol.gd
+++ cinoritmo/01/player_contrrol.gd
@@ -4,3 +4,10 @@ func _physics_process(delta: float) -> void:
 	#going right 'ere
 	velocity.x = 90
 	move_and_slide()
+
+func _on_end_area_body_entered(body: Node2D) -> void:
+	print("Exited") # Replace with function body.
+
+
+func _on_start_area_body_entered(body: Node2D) -> void:
+	print("Entered")
