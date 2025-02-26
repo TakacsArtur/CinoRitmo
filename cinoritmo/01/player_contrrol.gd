1c287c7babeeaba2f6ef276f990cf8a27ce90d2c
Takacs Artur <takacs.artur@outlook.com>
2025-02-26 07:47:14 +0100

    the basic mechanics work now

--- cinoritmo/01/player_contrrol.gd
+++ cinoritmo/01/player_contrrol.gd
@@ -3,6 +3,11 @@ var curentVelocity = 200
 var score = 0
 var inside = false
 var intopRow = false
+var inmidRow = false
+var inbotRow = false
+var correctplay = false
+#mistakes are simply stronger than the correct marker
+var mistake = false
 
 func _physics_process(delta: float) -> void:
 	#going right 'ere
@@ -11,6 +16,7 @@ func _physics_process(delta: float) -> void:
 
 func _on_end_area_body_entered(body: Node2D) -> void:
 	curentVelocity = 0
+	print(score)
 	
 func _on_start_area_body_entered(body: Node2D) -> void:
 	print("Entered")
@@ -23,16 +29,70 @@ func _on_top_row_body_exited(body: Node2D) -> void:
 	intopRow = false
 	print("ExitedTopRow")
 
-func _process(delta: float) -> void:
-	if intopRow:
-		if Input.is_action_pressed("toprow"):
-			if Input.is_action_pressed("strum"):
-				print("moreinfo")
-				increaseScore()	
+func _on_mid_row_body_entered(body: Node2D) -> void:
+	inmidRow = true
+	print("EnteredMidRow")
 
-func faultyPlay() -> void:
-	pass
+func _on_mid_row_body_exited(body: Node2D) -> void:
+	inmidRow = false
+	print("ExitedMidRow")
 	
-func increaseScore() -> void:
-	score += 100
+func _on_bottom_row_body_entered(body: Node2D) -> void:
+	inbotRow = true
+	print("EnteredBotRow")
+
+func _on_bottom_row_body_exited(body: Node2D) -> void:
+	inbotRow = false
+	print("ExitedBotRow")
+	
+func _process(delta: float) -> void:
+	#firstly we check if the strumbar is engaged SHOULD IT BE
+	if Input.is_action_pressed("strum"):
+		#toprow logic
+		if intopRow:
+			if Input.is_action_pressed("toprow"):
+				correctplay = true
+				mistake = false
+				print("Correct play detected top")
+			else:
+				correctplay = false
+				mistake = true
+				print("Mistake detected bot")
+			#midrow logic
+		if inmidRow:
+			if Input.is_action_pressed("midrow"):
+				correctplay = true
+				mistake = false
+				print("Correct play detected mid")
+			else:
+				correctplay = false
+				mistake = true
+				print("Mistake detected mid")
+		#botrow logic
+		if inbotRow:
+			if Input.is_action_pressed("botrow"):
+				correctplay = true
+				mistake = false
+				print("Correct play detected bot")
+			else:
+				correctplay = false
+				mistake = true
+				print("Mistake detected bot")
+	calculateScore()
+
+	##reset the mistake/correctplay, if none of the conditions apply
+	mistake = false
+	correctplay = false
+
+	
+
+	
+func calculateScore() -> void:
+	if mistake == true:
+		score -= 45
+	if mistake == false and correctplay == true:
+		score += 50
+	else:
+		score +=0
 	print(score)
+	
